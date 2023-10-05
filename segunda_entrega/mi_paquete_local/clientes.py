bd6d2f8e75bb069da8e2746c6883a4d5317f5ffe
Candela Salvatto <cande_salvatto12@hotmail.com>
2023-10-05 19:13:12 -0300

    guardando cambios para la entrega

--- segunda_entrega/mi_paquete_local/clientes.py
+++ segunda_entrega/mi_paquete_local/clientes.py
@@ -13,68 +13,70 @@ class Cliente(Persona):
         return f"Cliente: {self.nombre} {self.apellido}\nEdad: {self.edad}\nGénero: {self.genero}\nDirección: {self._direccion}\nTeléfono: {self._telefono}\nSaldo: ${self._saldo}"
 
       
-def comprar(self):
-    producto = input("¿Que desea comprar?")
-    precio = 1000
-    print(f"{producto} cuesta ${precio}")
-    
-    confirmar_compra = input("¿Desea comprar este producto? SI/NO: ")
-    if confirmar_compra.upper() == "NO":
-        print("Muchas gracias por su visita.")
-        return
-        
-    # while True:
-    #     if precio <= self._saldo:
-    #         self._saldo -= precio
-    #         print(f"Tu compra de {producto}ha sido realizada con éxito! Saldo restante: ${self._saldo}")
-    #         break
-    #     else:
-    #         print("Saldo insuficiente para realizar la compra.")
-    #         intentar_nuevamente = input("¿Desea intentar nuevamente? SI/NO: ")
-    #         if intentar_nuevamente.upper() == "NO":
-    #             break
+    def comprar(self):
+        producto = input("¿Que desea comprar?")
+        precio = 1000
+        print(f"{producto} cuesta ${precio}")
 
-def cancelar_compra():
-        print('Tu compra ha sido cancelada')
-        
-        
-def ingresar(lista_de_clientes):
-    while True:
-        nombre = input("Ingrese su nombre de cliente: ")
-        saldo = float(input("Ingrese su saldo: "))
+        while True:
+            confirmar_compra = input("¿Desea comprar este producto? SI/NO: ")
+            if confirmar_compra.upper() == "NO":
+                print("Muchas gracias por su visita.")
+                break
 
-        for cliente in lista_de_clientes:
-            if cliente['nombre'] == nombre and cliente['saldo'] == saldo:
-                print("Sesión Activa")
-                return 
+            if precio <= self._saldo:
+                self._saldo -= precio
+                print(f"Tu compra de {producto} ha sido realizada con éxito! Saldo restante: ${self._saldo}")
+                break
             else:
-                print("Verifique su nombre o saldo.")
+                print("Saldo insuficiente para realizar la compra.")
                 intentar_nuevamente = input("¿Desea intentar nuevamente? SI/NO: ")
                 if intentar_nuevamente.upper() == "NO":
                     break
-                
+
+def cancelar_compra():
+            print('Tu compra ha sido cancelada')
+            
+            
+def ingresar():
+        while True:
+            nombre = input("Ingrese su nombre de cliente: ")
+            saldo = float(input("Ingrese su saldo: "))
+
+            for cliente in lista_de_clientes:
+                if cliente.nombre == nombre and cliente._saldo == saldo:
+                    print("Sesión Activa")
+                    return cliente
+                else:
+                    print("Verifique su nombre o saldo.")
+                    intentar_nuevamente = input("¿Desea intentar nuevamente? SI/NO: ")
+                    if intentar_nuevamente.upper() == "NO":
+                        break
+            return None
+                    
 def registrar():
-        nombre = input("Ingrese su nombre: ")
-        apellido = input("Ingrese su apellido: ")
-        edad = int(input("Ingrese su edad: "))
-        genero = input("Ingrese su género: ")
-        direccion = input("Ingrese su dirección: ")
-        telefono = int(input("Ingrese su teléfono: "))
-        saldo = float(input("Ingrese su saldo: "))
-        
-        cliente = {
-        "nombre": nombre,
-        "apellido": apellido,
-        "edad": edad,
-        "genero": genero,
-        "direccion": direccion,
-        "telefono": telefono,
-        "saldo": saldo
-    }
-        cliente = Cliente(nombre, apellido, edad, genero, direccion, telefono, saldo)
-        lista_de_clientes.append(cliente)
-        print("Ha sido registrado con éxito.")
-        print(cliente)
+            nombre = input("Ingrese su nombre: ")
+            apellido = input("Ingrese su apellido: ")
+            edad = int(input("Ingrese su edad: "))
+            genero = input("Ingrese su género: ")
+            direccion = input("Ingrese su dirección: ")
+            telefono = int(input("Ingrese su teléfono: "))
+            saldo = float(input("Ingrese su saldo: "))
+            
+            cliente = {
+            "nombre": nombre,
+            "apellido": apellido,
+            "edad": edad,
+            "genero": genero,
+            "direccion": direccion,
+            "telefono": telefono,
+            "saldo": saldo
+        }
+            cliente = Cliente(nombre, apellido, edad, genero, direccion, telefono, saldo)
+            lista_de_clientes.append(cliente)
+            print("Ha sido registrado con éxito.")
+            print(cliente)
+            return cliente
         
 
         
--- segunda_entrega/mi_paquete_local/tienda.py
+++ segunda_entrega/mi_paquete_local/tienda.py
@@ -1,21 +1,25 @@
 import clientes
 
+
 menu_tienda = True
 while menu_tienda:
-        opcion= int(input('Bienvenido! ¿Qué quieres hacer?\n 1-Registrarme \n 2-Ingresar \n 3-Comprar\n 4-Cancelar compra\n 5-Salir\n'))
-        if opcion == 1:
-            clientes.registrar()
-        elif opcion == 2: 
-            clientes.ingresar()
-        elif opcion == 3: 
-            clientes.comprar()
-        elif opcion == 4: 
-            clientes.cancelar_compra()            
-        elif opcion == 5:
-            print("Gracias por visitarnos! Hasta Pronto!")
-            menu_tienda = False
-        else: 
-            print("Eliga una opción válida")
-        
+    opcion = int(input('Bienvenido! ¿Qué quieres hacer?\n 1-Registrarme \n 2-Ingresar \n 3-Comprar\n 4-Cancelar compra\n 5-Salir\n'))
+    if opcion == 1:
+        cliente = clientes.registrar()  #Instancio mi cliente para poder usar los metodos
+    elif opcion == 2:
+        cliente = clientes.ingresar()  
+    elif opcion == 3:
+        if cliente is not None:
+            cliente.comprar()
+        else:
+            print("Debes ingresar o registrarte primero.")
+    elif opcion == 4:
+        cliente = clientes.cancelar_compra()
+    elif opcion == 5:
+        print("Gracias por visitarnos! Hasta Pronto!")
+        menu_tienda = False
+    else:
+        print("Elige una opción válida")
+
 
 
